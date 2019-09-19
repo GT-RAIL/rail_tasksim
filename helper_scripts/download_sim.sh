a0d5a90e4802a543f035f54cfb7513846d44c0bd
Angel <adaruna3@gatech.edu>
2019-09-19 17:41:31 -0400

    updates to download script for meta repo usage

--- helper_scripts/download_sim.sh
+++ helper_scripts/download_sim.sh
@@ -11,9 +11,9 @@ fi
 base_url="http://virtual-home.org/release/simulator/"
 url=$base_url$name_script".zip"
 echo "Downloading"
-wget $url
-mv $name_script.zip simulation/
-cd simulation
+#wget $url
+mv $name_script.zip ./rail_tasksim/simulation/
+cd ./rail_tasksim/simulation/
 unzip $name_script.zip
-cd ..
+cd ../../
 echo "Executable moved to simulation folder"
